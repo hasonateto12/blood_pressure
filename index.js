80ac764ba326125d308dc777070c625914b64316
hasonateto <hassunateto3@gmail.com>
2025-02-20 06:02:07 +0200

    index file blood_perssure

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-console.log('Happy developing ✨')
+const port = 2135;
+const express = require('express');
+const app = express();
+app.use(express.json());
+
+const bodyParser = require('body-parser');
+const path = require("path");
+app.use(bodyParser.urlencoded({extended: false}));
+
+const bodyParser = require('body-parser');
+const path = require("path");
+app.use(bodyParser.urlencoded({extended: false}));
+
+let db_M = require('./database');
+global.db_pool = db_M.pool;
+
+
+app.listen(port, () => {            //server starts listening for any attempts from a client to connect at port: {port}
+    console.log(`Now listening on port http://localhost:${port}`);
+});
