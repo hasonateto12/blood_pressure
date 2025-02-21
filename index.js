cc8946c58e1baea4013fadf73caa0c39bdf23344
hasonateto <hassunateto3@gmail.com>
2025-02-21 04:33:52 +0200

    Add user routes and integrate Users_R router

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// npm i express body-parser ejs htmlspecialchars mysql2  slashes@2.0.0
 const port = 2135;
 const express = require('express');
 const app = express();
@@ -7,13 +8,15 @@ const bodyParser = require('body-parser');
 const path = require("path");
 app.use(bodyParser.urlencoded({extended: false}));
 
-const bodyParser = require('body-parser');
-const path = require("path");
-app.use(bodyParser.urlencoded({extended: false}));
-
 let db_M = require('./database');
 global.db_pool = db_M.pool;
 
+global.htmlspecialchars = require('htmlspecialchars');
+// const { addSlashes, stripSlashes } = require('slashes');
+
+
+const Users_R = require('./Routers/Users_R');
+app.use('/U/',Users_R);
 
 app.listen(port, () => {            //server starts listening for any attempts from a client to connect at port: {port}
     console.log(`Now listening on port http://localhost:${port}`);
