727ebbbdb308b45e7fe1966ffa974f0126aed464
hasonateto12 <hassunateto3@gmail.com>
2026-02-27 04:32:44 +0200

    Add Docker + compose with MySQL and env config

--- gen_params.js
+++ gen_params.js
@@ -1,4 +1,4 @@
-exports.HOST        ="localhost";
-exports.USER        ="root";
-exports.PASSWORD    ="";
-exports.DATABASE    ="blood_pressure";
+exports.HOST     = process.env.DB_HOST || "localhost";
+exports.USER     = process.env.DB_USER || "root";
+exports.PASSWORD = process.env.DB_PASS || "";
+exports.DB       = process.env.DB_NAME || "blood_pressure";
